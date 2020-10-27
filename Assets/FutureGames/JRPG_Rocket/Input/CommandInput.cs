5436a48bdcbad0dd32766ca510218a437fee3202
Fredrik Henriksen Lövlie <fredriklovlie@gmail.com>
2020-10-27 11:33:45 +0100

    ProjectPush

---
+++ Assets/FutureGames/JRPG_Rocket/Input/CommandInput.cs
@@ -0,0 +1,286 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/FutureGames/JRPG_Rocket/Input/CommandInput.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @CommandInput : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @CommandInput()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""CommandInput"",
+    ""maps"": [
+        {
+            ""name"": ""PlayerOne"",
+            ""id"": ""7ca19104-b02d-4fc8-8d00-9b3c8faa9e46"",
+            ""actions"": [
+                {
+                    ""name"": ""ExecuteCommand"",
+                    ""type"": ""Button"",
+                    ""id"": ""43cefa7e-0b57-483a-bde4-7aa20f2e247b"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Button"",
+                    ""id"": ""fc2e8e86-41a4-403f-8b5c-08cd0bbef838"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""e5cd42d3-6e38-42dd-82fa-8d2dd61be593"",
+                    ""path"": ""<Keyboard>/enter"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Mouse & Keyboard"",
+                    ""action"": ""ExecuteCommand"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""51dac4a1-1e38-4590-94cb-3038ecb6edb3"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""12006997-62c4-4061-a5b8-a04978021509"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Mouse & Keyboard"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""4995574c-496d-4d4d-9eea-5d784da01f61"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Mouse & Keyboard"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""187cbff1-a756-4ccd-b9d1-c8935165bb59"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Mouse & Keyboard"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""0ba2e577-a83e-4f8e-9066-a894b75153fc"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Mouse & Keyboard"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""ArrowKeys"",
+                    ""id"": ""c415e284-9634-4bee-8d6e-000fec8b3a5a"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Mouse & Keyboard"",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""06258542-3fa4-435b-9528-4c10b57ead67"",
+                    ""path"": ""<Keyboard>/upArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Mouse & Keyboard"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""d86f4aae-4750-40de-9f9b-3a9cb0a75100"",
+                    ""path"": ""<Keyboard>/downArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Mouse & Keyboard"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""56f29f6d-6859-4db2-9a59-1245e9c3555c"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Mouse & Keyboard"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""e4293d03-a8ad-48ef-b2a3-de19e183f6be"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Mouse & Keyboard"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Mouse & Keyboard"",
+            ""bindingGroup"": ""Mouse & Keyboard"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Mouse>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                },
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+        // PlayerOne
+        m_PlayerOne = asset.FindActionMap("PlayerOne", throwIfNotFound: true);
+        m_PlayerOne_ExecuteCommand = m_PlayerOne.FindAction("ExecuteCommand", throwIfNotFound: true);
+        m_PlayerOne_Move = m_PlayerOne.FindAction("Move", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // PlayerOne
+    private readonly InputActionMap m_PlayerOne;
+    private IPlayerOneActions m_PlayerOneActionsCallbackInterface;
+    private readonly InputAction m_PlayerOne_ExecuteCommand;
+    private readonly InputAction m_PlayerOne_Move;
+    public struct PlayerOneActions
+    {
+        private @CommandInput m_Wrapper;
+        public PlayerOneActions(@CommandInput wrapper) { m_Wrapper = wrapper; }
+        public InputAction @ExecuteCommand => m_Wrapper.m_PlayerOne_ExecuteCommand;
+        public InputAction @Move => m_Wrapper.m_PlayerOne_Move;
+        public InputActionMap Get() { return m_Wrapper.m_PlayerOne; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerOneActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerOneActions instance)
+        {
+            if (m_Wrapper.m_PlayerOneActionsCallbackInterface != null)
+            {
+                @ExecuteCommand.started -= m_Wrapper.m_PlayerOneActionsCallbackInterface.OnExecuteCommand;
+                @ExecuteCommand.performed -= m_Wrapper.m_PlayerOneActionsCallbackInterface.OnExecuteCommand;
+                @ExecuteCommand.canceled -= m_Wrapper.m_PlayerOneActionsCallbackInterface.OnExecuteCommand;
+                @Move.started -= m_Wrapper.m_PlayerOneActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_PlayerOneActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_PlayerOneActionsCallbackInterface.OnMove;
+            }
+            m_Wrapper.m_PlayerOneActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @ExecuteCommand.started += instance.OnExecuteCommand;
+                @ExecuteCommand.performed += instance.OnExecuteCommand;
+                @ExecuteCommand.canceled += instance.OnExecuteCommand;
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+            }
+        }
+    }
+    public PlayerOneActions @PlayerOne => new PlayerOneActions(this);
+    private int m_MouseKeyboardSchemeIndex = -1;
+    public InputControlScheme MouseKeyboardScheme
+    {
+        get
+        {
+            if (m_MouseKeyboardSchemeIndex == -1) m_MouseKeyboardSchemeIndex = asset.FindControlSchemeIndex("Mouse & Keyboard");
+            return asset.controlSchemes[m_MouseKeyboardSchemeIndex];
+        }
+    }
+    public interface IPlayerOneActions
+    {
+        void OnExecuteCommand(InputAction.CallbackContext context);
+        void OnMove(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/FutureGames/JRPG_Rocket/Input/InputPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace FutureGames.JRPG_Rocket
+{
+    public class InputPlayer : MonoBehaviour
+    {
+        private CommandInput CommandInput = null;
+
+        private void Awake()
+        {
+            CommandInput = new CommandInput();        
+        }
+
+        private void OnEnable()
+        {
+            
+        }
+
+        private void OnDisable()
+        {
+            
+        }
+    }   
+}
